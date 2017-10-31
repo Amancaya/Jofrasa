68a1b3dc5e3193afab0258b2a5280172c8bffeb2
Amancaya <koda01996gmail.com>
2017-10-31 17:17:18 -0400

    Implementacion de Retrofit2 [issue2]

--- app/src/main/java/free/tech/jofrasa/Interface/ApiInterface.java
+++ app/src/main/java/free/tech/jofrasa/Interface/ApiInterface.java
@@ -1,8 +1,13 @@
 package free.tech.jofrasa.Interface;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
  * Created by root on 31-10-17.
  */
 
 public interface ApiInterface {
+//    @GET("location/listLodgings/")
+//    Call<HotelMessage> getHotelMessageCall();
 }
